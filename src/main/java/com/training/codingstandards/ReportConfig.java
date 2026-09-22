98ffc207758b2f9315a54f8d1b97c9796f8d866a
Pattabhi45 <ramamuniyappagari.pattabhiraman@bmwtechworks.in>
2026-09-22 17:33:24 +0530

    Update ReportConfig.java

--- src/main/java/com/training/codingstandards/ReportConfig.java
+++ src/main/java/com/training/codingstandards/ReportConfig.java
@@ -1,17 +1,20 @@
 package com.training.codingstandards;
 
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReportConfig {
+public final class ReportConfig {
 
-    public static List<Employee> CACHE = new ArrayList<Employee>();
+    public static final List<Employee> CACHE = new ArrayList<>();
 
-    public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static String OUTPUT_SHEET = "Payroll";
-    public static String OUTPUT_SHEET_2 = "Payroll";
+    public static final String OUTPUT_SHEET = "Payroll";
+    public static final String OUTPUT_SHEET_2 = "Payroll";
 
-    public static final String DEFAULT_PASSWORD = "P@ssw0rd!";
+    public static final String DEFAULT_PASSWORD = System.getenv().getOrDefault("APP_ADMIN_PASSWORD", "");
+
+    private ReportConfig() {
+    }
 }
